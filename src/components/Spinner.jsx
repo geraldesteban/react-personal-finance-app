60005178e01f1f7ce6c22d253ad3bb22cf40ea3b
geraldesteban <wakin.esteban@gmail.com>
2025-08-28 00:06:40 +0800

    add loading spinner

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,15 @@
+export default function Spinner() {
+  return (
+    <div className="flex items-center justify-center h-full w-full">
+      <div
+        className="w-[40px] aspect-square rounded-full animate-spin"
+        style={{
+          background: `radial-gradient(farthest-side, #201F24 94%, #0000) top/10px 10px no-repeat,
+                       conic-gradient(#0000 30%, #201F24)`,
+          WebkitMask:
+            "radial-gradient(farthest-side, #0000 calc(100% - 5px), #000 0)",
+        }}
+      ></div>
+    </div>
+  );
+}
