5277c306f83d0b7df7c8850c9bb54d886a88a54c
geraldesteban <wakin.esteban@gmail.com>
2025-08-29 02:01:22 +0800

    fix position from desktop to tablet and mobile

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -15,21 +15,23 @@ function Sidebar() {
 
   return (
     <div
-      className={`bg-grey-900 flex flex-col justify-between py-10 rounded-tr-xl rounded-br-xl
+      className={`bg-grey-900 flex flex-col h-full justify-between py-10 rounded-tr-xl rounded-br-xl
     transition-[width] duration-200
-    ${active ? "w-[500px]" : "w-[110px]"} lg:hidden`}
+    ${
+      active ? "w-[300px]" : "w-[110px]"
+    } lg:w-full lg:py-0 lg:rounded-br-none lg:rounded-tl-xl lg:pt-2 lg:px-5`}
     >
       {/* Navigation Sidebar */}
       <nav className="text-grey-300">
         {/* Logo */}
         <NavLink>
           {active ? (
-            <LogoLargeIcon className="mb-16 ml-10" />
+            <LogoLargeIcon className="mb-16 ml-10 lg:hidden lg:mb-0 lg:ml-0" />
           ) : (
-            <LogoSmallIcon className="mb-16 ml-10" />
+            <LogoSmallIcon className="mb-16 ml-10 lg:hidden lg:mb-0 lg:ml-0" />
           )}
         </NavLink>
-        <ul className="flex flex-col text-[16px]">
+        <ul className="flex flex-col text-[16px] lg:flex-row lg:justify-between">
           <SidebarNavigations
             to={"/overview"}
             label="Overview"
@@ -59,12 +61,11 @@ function Sidebar() {
             label="Recurring Bills"
             icon={RecurringBillsIcon}
             isActive={active}
-            width="w-28"
           />
         </ul>
       </nav>
       <button
-        className={`group font-bold flex items-center py-5`}
+        className={`group font-bold flex items-center py-5 lg:hidden`}
         onClick={() => setActive(!active)}
       >
         <MinimizeLeftIcon
@@ -73,7 +74,7 @@ function Sidebar() {
           } group-hover:text-grey-100`}
         />
         <span
-          className={`text-grey-300 ${
+          className={`whitespace-nowrap text-grey-300 ${
             active ? "block w-32" : "hidden w-32"
           } group-hover:text-grey-100`}
         >
--- src/components/Sidebar/SidebarNavigations.jsx
+++ src/components/Sidebar/SidebarNavigations.jsx
@@ -5,7 +5,6 @@ export default function SidebarNavigations({
   label,
   icon: Icon,
   isActive,
-  width,
 }) {
   const location = useLocation();
   const currentPage = location.pathname.split("/").filter(Boolean).pop();
@@ -16,23 +15,30 @@ export default function SidebarNavigations({
     <li
       className={`group text-grey-300 flex items-center rounded-tr-xl rounded-br-xl relative mr-5 ${
         active ? "bg-beige-100" : ""
-      } ${isActive ? "mr-5" : "mr-0"}`}
+      } ${
+        isActive ? "mr-5" : "mr-0"
+      } lg:rounded-br-none lg:rounded-tl-xl lg:mr-0 lg:px-5`}
     >
       <div
-        className={`absolute left-0 top-0 h-full w-1 ${
+        className={`absolute left-0 h-full w-1 ${
           active ? "bg-green" : ""
-        }`}
+        } lg:h-1 lg:w-full lg:bottom-0`}
       ></div>
-      <NavLink className="text-[16px] font-bold flex pl-10 py-5" to={to}>
+      <NavLink
+        className="font-myFontBold text-[16px] flex pl-10 py-5 lg:flex-col lg:pl-0 lg:items-center lg:py-2"
+        to={to}
+      >
         <Icon
           className={`w-6 h-6 mr-9 ${active ? "text-green" : ""} ${
             active ? "" : "group-hover:text-grey-100"
-          }`}
+          } lg:mb-2 lg:mr-0 lg:w-5 lg:h-5`}
         />
         <span
-          className={`${isActive ? `block ${width}` : `hidden ${width}`} ${
+          className={`whitespace-nowrap ${isActive ? `block` : `hidden`} ${
             active ? "text-grey-900" : ""
-          } text-[16px] ${active ? "" : "group-hover:text-grey-100"}`}
+          } text-[16px] ${active ? "" : "group-hover:text-grey-100"} ${
+            isActive ? `block` : `hidden`
+          } lg:text-[12px] lg:block sm:hidden`}
         >
           {label}
         </span>
