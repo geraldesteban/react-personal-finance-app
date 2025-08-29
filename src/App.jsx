a38e54ae2f87314f397e57a02653c4f254663014
geraldesteban <wakin.esteban@gmail.com>
2025-08-30 00:54:06 +0800

    add query client provider

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,26 @@ import Transactions from "./pages/Transactions";
 import Budgets from "./pages/Budgets";
 import Pots from "./pages/Pots";
 import RecurringBills from "./pages/RecurringBills";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AppLayout />}>
-          <Route index element={<Navigate replace to="overview" />} />
-          <Route path="overview" element={<Overview />} />
-          <Route path="transactions" element={<Transactions />} />
-          <Route path="budgets" element={<Budgets />} />
-          <Route path="pots" element={<Pots />} />
-          <Route path="recurringbills" element={<RecurringBills />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Navigate replace to="overview" />} />
+            <Route path="overview" element={<Overview />} />
+            <Route path="transactions" element={<Transactions />} />
+            <Route path="budgets" element={<Budgets />} />
+            <Route path="pots" element={<Pots />} />
+            <Route path="recurringbills" element={<RecurringBills />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
