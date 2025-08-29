ed892afbcd4743e098231d492b01e75572a35b96
geraldesteban <wakin.esteban@gmail.com>
2025-08-30 00:55:21 +0800

    change color of spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,11 +1,11 @@
 export default function Spinner() {
   return (
-    <div className="flex items-center justify-center min-h-screen w-full">
+    <div className="flex items-center justify-center">
       <div
         className="w-[40px] aspect-square rounded-full animate-spin"
         style={{
-          background: `radial-gradient(farthest-side, #201F24 94%, #0000) top/10px 10px no-repeat,
-                       conic-gradient(#0000 30%, #201F24)`,
+          background: `radial-gradient(farthest-side, #277C78 94%, #0000) top/10px 10px no-repeat,
+                       conic-gradient(#0000 30%, #277C78)`,
           WebkitMask:
             "radial-gradient(farthest-side, #0000 calc(100% - 5px), #000 0)",
         }}
