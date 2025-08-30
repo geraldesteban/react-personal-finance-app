f06f732d1ba5340d352fb6c08c301ee35a29d68c
geraldesteban <wakin.esteban@gmail.com>
2025-08-30 20:34:12 +0800

    add login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import AuthenticationLayout from "../components/Authentication/AuthenticationLayout";
+
+export default function Login() {
+  return (
+    <div className="bg-[#F8F4F0]">
+      <AuthenticationLayout />
+    </div>
+  );
+}
