ebe380342bc14dfe72993c8ac9a1ae3606a8e0d5
geraldesteban <wakin.esteban@gmail.com>
2025-08-31 05:56:27 +0800

    add signup ui

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,9 @@
+import AuthenticationSignout from "../components/Authentication/AuthenticationSignup";
+
+export default function Login() {
+  return (
+    <div className="flex justify-center items-center bg-[#F2F3F7] h-screen">
+      <AuthenticationSignout />
+    </div>
+  );
+}
