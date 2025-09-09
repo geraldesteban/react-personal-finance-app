685c18480ec24f3d6863761e414f10c174d3420b
geraldesteban <wakin.esteban@gmail.com>
2025-09-10 04:53:14 +0800

    ui: create reusable search

---
+++ src/ui/Search.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import SearchIcon from "../assets/icon-search.svg?react";
+import { useSearchParams } from "react-router-dom";
+
+export default function RecurringBillsSearch() {
+  const [query, setQuery] = useState("");
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    const search = searchParams.get("search") || "";
+    setQuery(search);
+  }, [searchParams]);
+
+  function getQuery(e) {
+    setQuery(e.target.value);
+    setSearchParams({ search: e.target.value });
+  }
+
+  return (
+    <form className="relative w-64 mr-2">
+      <input
+        className="font-myFontRegular text-[14px] w-full py-2.5 pl-5 border border-beige-500 rounded-lg outline-none focus:border-grey-900"
+        type="search"
+        placeholder="Search bills"
+        value={query}
+        onChange={getQuery}
+      />
+      <SearchIcon className="absolute inset-y-4 right-5 flex items-center text-grey-900" />
+    </form>
+  );
+}
