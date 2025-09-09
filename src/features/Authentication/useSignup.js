19ba8b6a4428e6eabd16d176528be243096b9608
geraldesteban <wakin.esteban@gmail.com>
2025-09-10 00:10:53 +0800

    feat: add reset when error

--- src/features/Authentication/useSignup.js
+++ src/features/Authentication/useSignup.js
@@ -7,6 +7,7 @@ export function useSignup() {
     mutate: signup,
     isPending: isSignup,
     error: errorSignup,
+    reset,
   } = useMutation({
     mutationFn: apiSignUp,
     onSuccess: () => {
@@ -14,6 +15,7 @@ export function useSignup() {
     },
     onError: (err) => {
       toast.error(err.message);
+      reset();
     },
   });
 
