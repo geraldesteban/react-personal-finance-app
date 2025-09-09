7eb4c1991b807584fc5148047fceef7e671e1d6a
geraldesteban <wakin.esteban@gmail.com>
2025-09-10 03:49:27 +0800

    refactor: remove label from select theme and budget category

--- src/ui/SelectBudgetCategory.jsx
+++ src/ui/SelectBudgetCategory.jsx
@@ -1,29 +1,22 @@
-import React from "react";
-
-export default function SelectBudgetCategory({ label, value, onChange }) {
+export default function SelectBudgetCategory({ value, onChange }) {
   return (
-    <>
-      <label className="block font-myFontBold text-[12px] text-grey-500 mb-2">
-        {label}
-      </label>
-      <select
-        name="budgets"
-        id="budgets"
-        className="w-full border border-beige-500 rounded-xl py-2 cursor-pointer pl-5 mb-5"
-        value={value}
-        onChange={onChange}
-      >
-        <option value="Entertainment">Entertainment</option>
-        <option value="Bills">Bills</option>
-        <option value="Groceries">Groceries</option>
-        <option value="Dining Out">Dining Out</option>
-        <option value="Transportation">Transportation</option>
-        <option value="Personal Care">Personal Care</option>
-        <option value="Education">Education</option>
-        <option value="Lifestyle">Lifestyle</option>
-        <option value="Shopping">Shopping</option>
-        <option value="General">General</option>
-      </select>
-    </>
+    <select
+      name="budgets"
+      id="budgets"
+      className="w-full border border-beige-500 rounded-xl py-2 cursor-pointer pl-5 mb-5 outline-none focus:border-grey-900"
+      value={value}
+      onChange={onChange}
+    >
+      <option value="Entertainment">Entertainment</option>
+      <option value="Bills">Bills</option>
+      <option value="Groceries">Groceries</option>
+      <option value="Dining Out">Dining Out</option>
+      <option value="Transportation">Transportation</option>
+      <option value="Personal Care">Personal Care</option>
+      <option value="Education">Education</option>
+      <option value="Lifestyle">Lifestyle</option>
+      <option value="Shopping">Shopping</option>
+      <option value="General">General</option>
+    </select>
   );
 }
--- src/ui/SelectThemeColor.jsx
+++ src/ui/SelectThemeColor.jsx
@@ -1,32 +1,27 @@
-export default function SelectThemeColor({ label, value, onChange }) {
+export default function SelectThemeColor({ value, onChange }) {
   return (
-    <>
-      <label className="block font-myFontBold text-grey-500 text-[12px] mb-2">
-        {label}
-      </label>
-      <select
-        name="theme"
-        id="theme"
-        className="w-full border border-grey-500 rounded-xl py-2 cursor-pointer pl-5 mb-5"
-        value={value}
-        onChange={onChange}
-      >
-        <option value="#277C78">Green</option>
-        <option value="#98908B">Beige</option>
-        <option value="#82C9D7">Cyan</option>
-        <option value="#626070">Navy</option>
-        <option value="#C94736">Red</option>
-        <option value="#826CB0">Purple</option>
-        <option value="#597C7C">Turquoise</option>
-        <option value="#93674F">Brown</option>
-        <option value="#934F6F">Magenta</option>
-        <option value="#3F82B2">Blue</option>
-        <option value="#97A0AC0">Grey</option>
-        <option value="#7F9161">Army</option>
-        <option value="#AF81BA">Pink</option>
-        <option value="#F2CDAC">Yellow</option>
-        <option value="#BE6C49">Orange</option>
-      </select>
-    </>
+    <select
+      name="theme"
+      id="theme"
+      className="w-full border border-grey-500 rounded-xl py-2 cursor-pointer pl-5 mb-5 outline-none focus:border-grey-900"
+      value={value}
+      onChange={onChange}
+    >
+      <option value="#277C78">Green</option>
+      <option value="#98908B">Beige</option>
+      <option value="#82C9D7">Cyan</option>
+      <option value="#626070">Navy</option>
+      <option value="#C94736">Red</option>
+      <option value="#826CB0">Purple</option>
+      <option value="#597C7C">Turquoise</option>
+      <option value="#93674F">Brown</option>
+      <option value="#934F6F">Magenta</option>
+      <option value="#3F82B2">Blue</option>
+      <option value="#97A0AC0">Grey</option>
+      <option value="#7F9161">Army</option>
+      <option value="#AF81BA">Pink</option>
+      <option value="#F2CDAC">Yellow</option>
+      <option value="#BE6C49">Orange</option>
+    </select>
   );
 }
