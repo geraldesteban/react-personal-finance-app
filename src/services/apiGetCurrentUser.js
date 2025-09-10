818585c0f0648da452ae05c01c5f42a4975db389
geraldesteban <wakin.esteban@gmail.com>
2025-09-10 16:19:51 +0800

    fix: api of get user so the delay from first load is remove

--- src/services/apiGetCurrentUser.js
+++ src/services/apiGetCurrentUser.js
@@ -2,11 +2,11 @@ import supabase from "./supabase";
 
 /* Get Current User */
 export async function GetCurrentUser() {
-  const { data: sessionData, error } = await supabase.auth.getSession();
+  const { data: session } = await supabase.auth.getSession();
+  if (!session.session) return null;
 
-  if (error) throw new Error("Failed to get session");
-  if (!sessionData.session || !sessionData.session.user)
-    throw new Error("User not logged in");
+  const { data, error } = await supabase.auth.getUser();
 
-  return sessionData.session.user;
+  if (error) throw new Error(error.message);
+  return data?.user;
 }
