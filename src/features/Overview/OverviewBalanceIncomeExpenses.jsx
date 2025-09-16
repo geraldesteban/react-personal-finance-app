2f928c16fcf9a3a43430f0bbe9117b6f56793253
geraldesteban <wakin.esteban@gmail.com>
2025-09-16 18:24:03 +0800

    refactor: remove error handling

--- src/features/Overview/OverviewBalanceIncomeExpenses.jsx
+++ src/features/Overview/OverviewBalanceIncomeExpenses.jsx
@@ -4,7 +4,7 @@ import Spinner from "../../ui/Spinner";
 import ErrorMessage from "../../ui/Spinner";
 
 export default function OverviewBalanceIncomeExpenses() {
-  const { balances, isBalances, errorBalances } = useBalance();
+  const { balances, isBalances } = useBalance();
 
   if (isBalances)
     return (
@@ -26,30 +26,6 @@ export default function OverviewBalanceIncomeExpenses() {
             Expenses
           </h2>
           <Spinner />
-        </div>
-      </div>
-    );
-
-  if (errorBalances)
-    return (
-      <div className="flex flex-wrap gap-5 mb-5 sm:flex-col mt-10">
-        <div className={`flex-1 p-7 rounded-xl text-white bg-grey-900 lg:p-5`}>
-          <h2 className={`font-myFontRegular text-[14px] text-white mb-2.5`}>
-            Current Balance
-          </h2>
-          <ErrorMessage error={errorBalances.message} />
-        </div>
-        <div className={`flex-1 p-7 rounded-xl bg-white lg:p-5`}>
-          <h2 className={`font-myFontRegular text-[14px] text-grey-500 mb-2.5`}>
-            Income
-          </h2>
-          <ErrorMessage />
-        </div>
-        <div className={`flex-1 p-7 rounded-xl bg-white lg:p-5`}>
-          <h2 className={`font-myFontRegular text-[14px] text-grey-500 mb-2.5`}>
-            Expenses
-          </h2>
-          <ErrorMessage error={errorBalances.message} />
         </div>
       </div>
     );
