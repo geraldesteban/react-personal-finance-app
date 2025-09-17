64d4b5a7cfef150dbef2e6808044ad2a3d86431f
geraldesteban <wakin.esteban@gmail.com>
2025-09-17 16:21:27 +0800

    refactor: remove unncessary logic and add comments

--- src/services/apiPot/apiAddMoneyToPot.js
+++ src/services/apiPot/apiAddMoneyToPot.js
@@ -7,73 +7,71 @@ export async function apiAddMoneyToPot({ pot_id, amount }) {
   const currentUser = await GetCurrentUser();
 
   /* Get current Balance of the User */
-  const { data: balanceData, error: balanceError } = await supabase
+  const { data: currentBalance, error: currentBalanceError } = await supabase
     .from("balances")
     .select("balance")
     .eq("user_id", currentUser.id)
     .single();
 
-  if (balanceError) throw new Error("Balance could not be Read");
+  /* Error get current Balance */
+  if (currentBalanceError) throw new Error("Balance could not be Read");
 
   /* No Balance */
-  if (!balanceData || balanceData.balance < 0)
+  if (!currentBalance || currentBalance.balance < 0)
     throw new Error("You currently have no Balance");
 
   /* Amount is higher than current Balance */
-  if (amount > balanceData.balance || amount < 0) {
+  if (amount > currentBalance.balance || amount < 0) {
     throw new Error(
       "Amount Pot money to be added is higher or lower than current Balance"
     );
   }
 
   /* Get current Pot Money of the User */
-  const { data: potData, error: potPotMoneyError } = await supabase
+  const { data: currentPotMoney, error: currentPotMoneyError } = await supabase
     .from("pots")
     .select("potName, potMoney, targetMoney")
     .eq("user_id", currentUser.id)
     .eq("id", pot_id)
     .single();
 
-  if (potPotMoneyError) throw new Error("Pot could not be Read");
+  /* Error get current Pot money */
+  if (currentPotMoneyError) throw new Error("Pot could not be Read");
 
   /* Invalid Amount */
   if (amount <= 0) throw new Error("Invalid Amount to Add");
 
   /* Amount is higher than Target money */
-  if (amount > potData.targetMoney)
+  if (amount > currentPotMoney.targetMoney)
     throw new Error("Amount is higher than Target money");
 
-  /* Updated Balance, Pot Money, and Target Money */
-  const updatedBalance = balanceData.balance - amount;
-  const updatedPotMoney = potData.potMoney + amount;
-
   /* Get Pot Name */
-  const potName = potData.potName;
+  const potName = currentPotMoney.potName;
 
   /* Update Pot Money and Target Money of the User */
   const { error: errorPot } = await supabase
     .from("pots")
     .update({
-      potMoney: updatedPotMoney,
+      potMoney: currentPotMoney.potMoney + amount,
     })
     .eq("user_id", currentUser.id)
     .eq("id", pot_id);
 
+  /* Error update Pot */
   if (errorPot)
     throw new Error("Pot Money or Target Money could not be updated");
 
   /* Update Balance of the User */
   const { error: errorBalance } = await supabase
     .from("balances")
-    .update({ balance: updatedBalance })
+    .update({ balance: currentBalance.balance - amount })
     .eq("user_id", currentUser.id);
 
+  /* Error update Balance */
   if (errorBalance) throw new Error("Balance could not be updated");
 
   return {
     amount,
     potName,
-    updatedBalance,
-    updatedPotMoney,
   };
 }
