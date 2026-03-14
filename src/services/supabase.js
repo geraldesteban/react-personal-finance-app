e2b473497769636f54342ababd0f7dc5688b080d
geraldesteban <wakin.esteban@gmail.com>
2026-03-15 01:05:43 +0800

    chore: url and key change

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,9 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://jnmcjrzftcwhddjyuoqt.supabase.co";
+export const supabaseUrl = "https://fikmrhvylchpmsiomobu.supabase.co";
 
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpubWNqcnpmdGN3aGRkanl1b3F0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY0NzUxNDQsImV4cCI6MjA3MjA1MTE0NH0.SkjCla583sIC07b6aO7uabO5X5XrAj-XHV-4AZmRIxU";
+const supabaseKey = "sb_publishable_ESSwXzj_CR-heT7Bg2TU9Q_BKncP_ny";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
